5f6464a01c7fa6a1b8413662c67419271ca4a520
AnnayaSharma <annayaboutique94@gmail.com>
2026-07-28 02:46:27 +0530

    Updated changed

--- frontend/src/api/jobs.js
+++ frontend/src/api/jobs.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE =
-  import.meta.env.VITE_API_URL || "http://localhost:8080";
+const RAW_API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const API_BASE = RAW_API_BASE.replace(/\/+$/, "");
 
 export async function getJobs(params) {
   const res = await axios.get(`${API_BASE}/api/jobs`, { params });
